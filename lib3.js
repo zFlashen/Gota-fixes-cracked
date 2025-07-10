6790c9dd41d70fb814308af92fd363609c62043b
zFlashen <79690021+zFlashen@users.noreply.github.com>
2025-07-10 23:30:09 +0300

    Create lib3.js

---
+++ lib3.js
@@ -0,0 +1,4 @@
+{
+    "endTime": "Mon, 12 Sep 2022 09:00:00 GMT",
+    "message": "Weekend sale is on again! You can get 46% off locked names by using code <b>WEEKENDSALE</b> in the \"Redeem Coupon\" section, and code <b>camlan</b> in the \"Support A Creator\" section during checkout at STORE_LINK. Offer ends Monday!"
+  }
